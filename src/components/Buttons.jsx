58b2e9721f502cadaaa8d01a1a08b16e57b5b75a
Nicole Wade <nwade0916@gmail.com.com>
2024-03-18 21:08:51 -0500

    completed assignment

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,26 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import React from 'react';
+import satData from '../components/satData.jsx';
+import './styling.css';
+
+
+
+
+function Buttons({ filterByType, setSat, displaySats }) {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+        <div className="flex-container"></div>
+      
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+    
+      <button onClick={() => setSat(satData)}>All Orbit</button>
     </div>
   );
-};
+}
 
 export default Buttons;
